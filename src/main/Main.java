c305410272931beab490ffb83f1d3b020454fe34
Iván <ivanjesuscp@outlook.com>
2025-04-29 10:16:08 +0200

    Pantallas menu y centro pokemon
    He mejorado visualmente la pantalla de menu y he creado la pantalla de centro pokemon

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,14 +13,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Menu.fxml")); // Creando el FXML manualmente nos permite poder acceder luego al controlador
         Parent root = loader.load();
-
        
         MenuController controller = loader.getController();
         controller.setPrimaryStage(primaryStage);
 
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Menu");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        /*Image image = new Image(getClass().getResourceAsStream("/imagenes/pokeball.png"));
+        primaryStage.getIcons().add(image);*/
     }
 
     public static void main(String[] args) {
